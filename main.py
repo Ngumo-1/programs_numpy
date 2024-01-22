90b74ec07b5805eed0b46ce06bd688db7ecb4c02
Ngumo-1 <morrisngumo7@gmail.com>
2024-01-22 21:52:38 +0300

    added generate_random_float method and tested

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import numpy as np
 from rand_int import generate_array_randomly
+from rand_floats import generate_random_float
 
-size=[5]
-xarr=generate_array_randomly(1,6, size)
-print(xarr)
+# size=[5]
+# xarr=generate_array_randomly(1,6, size)
+# print(xarr)
+shape=5
+float_s=generate_random_float(shape)
+print(float_s)
---
+++ rand_floats.py
@@ -0,0 +1,6 @@
+import numpy as np
+from numpy import random
+
+def generate_random_float(shape_of_array_to_be_generated):
+    arr=np.random.rand(shape_of_array_to_be_generated)
+    return arr
