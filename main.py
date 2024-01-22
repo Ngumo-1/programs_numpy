da293dbbab6bc5d723be4b1facc29868c3761a0a
Ngumo-1 <morrisngumo7@gmail.com>
2024-01-22 21:34:05 +0300

    created a rand_int file for holding methods and also tested if the file's methods worked well on the main file examples

---
+++ main.py
@@ -0,0 +1,6 @@
+import numpy as np
+from rand_int import generate_array_randomly
+
+size=[5]
+xarr=generate_array_randomly(1,6, size)
+print(xarr)
---
+++ rand_int.py
@@ -0,0 +1,12 @@
+import numpy as np
+from numpy import random
+
+# arr=np.array([1,2,3,4,5])
+# print(arr)
+#
+# xarr=np.random.randint(1,6, size=([5]))
+# print(xarr)
+
+def generate_array_randomly(start_number, end_number, size):
+    arr=np.random.randint(start_number, end_number, size=size)
+    return arr
