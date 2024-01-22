e60a46752387d4b1294a4090bddd60a787a1938b
Ngumo-1 <morrisngumo7@gmail.com>
2024-01-22 22:37:38 +0300

    method for selecting random elements of arrays to form new random arrays

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from rand_int import (generate_array_randomly,
                       generate_number_randomly,
                       random_no_frm_given_range)
 from rand_floats import generate_random_float
+from rand_choice import random_choice
 
 # size=[5]
 # xarr=generate_array_randomly(1,6, size)
@@ -18,6 +19,15 @@ from rand_floats import generate_random_float
 #
 # rand_dice_face_label=generate_number_randomly(1, 6)
 # print(rand_dice_face_label)
-size_tuple=(3,3,3)
-xarr=random_no_frm_given_range(1,3, size_tuple)
+# size_tuple=(3,3,3)
+# xarr=random_no_frm_given_range(1,3, size_tuple)
+# print(xarr)
+
+arr=np.array([1,2,3,4,5,6,7,8,9])
+# xarr=np.random.choice(arr, )
+shape=(2,2,2)
+
+
+
+xarr=random_choice(arr,shape)
 print(xarr)
---
+++ rand_choice.py
@@ -0,0 +1,5 @@
+import numpy as np
+from numpy import random
+
+def random_choice(array, shape):
+    return np.random.choice(array, shape)
